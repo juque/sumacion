a9b4a35c148606fa51d882dbd0e4fc1c56d97066
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2025-03-17 13:08:13 -0300

    alfa1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import Item from './components/Item';
 import './App.css'
 
+function generateRandomNumber() {
+  const number = Math.random() * 30 + 1;
+  return Math.floor(number);
+}
+
+function generateSumandos() {
+  const sumando1 = generateRandomNumber();
+  const sumando2 = Math.floor(Math.random() * sumando1) + 1;
+  return {id: crypto.randomUUID(), sumando1, sumando2 }
+}
+
+
 function App() {
 
-  const datos = [
-    {id: 1, name: 'Ele 1'},
-    {id: 1, name: 'Ele 2'},
-    {id: 3, name: 'Ele 3'}
-  ]
+  const datos = [];
 
   const itemsRenderizados = [];
 
+  for( let i = 0; i < 9; i++) {
+      const randomSum = generateSumandos();
+      datos.push(randomSum);
+  }
+
   for (let i = 0; i < datos.length; i++) {
     const elemento = datos[i];
-    itemsRenderizados.push(<Item key={elemento.id} id={elemento.id} name={elemento.name} />);
+    itemsRenderizados.push(<Item key={elemento.id} sumando1={elemento.sumando1} sumando2={elemento.sumando2} />);
   }
 
   return (
     <>
       <h1>Sumas</h1>
-      { itemsRenderizados }
+      <div className="wrap-container">
+        { itemsRenderizados }
+      </div>
     </>
   )
 }
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
-const Item = ({id, name}) => {
+const Item = ({sumando1, sumando2}) => {
+
   return (
-    <div>
-        <strong>ID:</strong> {id} <strong>name:</strong> { name }
-    </div>
+
+			<div className="addition-container">
+      <div className="inner-container">
+				<div className="row">
+					<div className="number top-number">{sumando1}</div>
+				</div>
+				<div className="row sum-row">
+					<div className="operator">+</div>
+					<div className="number bottom-number">{sumando2}</div>
+				</div>
+        </div>
+			</div>
+
   )
 }
 
