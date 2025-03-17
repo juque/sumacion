cc482e4da9235dc3d88fc0b8f02afa44d5db039e
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2025-03-17 12:17:53 -0300

    initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Item = ({id, name}) => {
+  return (
+    <div>
+        <strong>ID:</strong> {id} <strong>name:</strong> { name }
+    </div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const datos = [
+    {id: 1, name: 'Ele 1'},
+    {id: 1, name: 'Ele 2'},
+    {id: 3, name: 'Ele 3'}
+  ]
+
+  const itemsRenderizados = [];
+
+  for (let i = 0; i < datos.length; i++) {
+    const elemento = datos[i];
+    itemsRenderizados.push(<Item key={elemento.id} id={elemento.id} name={elemento.name} />);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Sumas</h1>
+      { itemsRenderizados }
     </>
   )
 }
